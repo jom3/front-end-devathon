b690d4fa5394591185a8c94438bce5d0061ea4cb
joams mogro gomez <joamsmg@gmail.com>
2024-07-21 17:23:29 -0400

    Revert "feat: input binding and provide http client added"
    This reverts commit a1a662d9ac14297f72371ceb36f37922498c4221.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,8 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter, withComponentInputBinding } from '@angular/router';
+import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes,withComponentInputBinding()),
-    provideHttpClient(withFetch())
-  ]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
