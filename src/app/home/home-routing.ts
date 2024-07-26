bec5cbbc203824150b5cb2a395a88c8f038766c3
joams mogro gomez <joamsmg@gmail.com>
2024-07-25 20:51:18 -0400

    feat: home route added

---
+++ src/app/home/home-routing.ts
@@ -0,0 +1,7 @@
+import { Routes } from "@angular/router";
+
+export const routes: Routes = [
+  {
+    path:'', loadComponent:()=>import('./pages/home-page/home-page.component').then(c=>c.HomePageComponent)
+  }
+]
