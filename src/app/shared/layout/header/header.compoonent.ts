2f4bc35cf222f7e7973276973ac495e1ca26a46f
joams mogro gomez <joamsmg@gmail.com>
2024-08-01 18:28:16 -0400

    feat: some functionalities added into header

--- src/app/shared/layout/header/header.compoonent.ts
+++ src/app/shared/layout/header/header.compoonent.ts
@@ -1,20 +1,39 @@
-import { Component, inject } from '@angular/core';
-import { MatDialog, MatDialogModule } from '@angular/material/dialog';
-import { LoginDialogComponent } from '../../components';
+import { Component, effect, inject, signal } from '@angular/core';
+import { MatDialog, MatDialogClose, MatDialogModule } from '@angular/material/dialog';
+import { LoginDialogComponent } from '../../../auth';
+import { Router, RouterLink } from '@angular/router';
+import { JwtService } from '../../services';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
   standalone: true,
-  imports: [MatDialogModule],
+  imports: [MatDialogModule,MatDialogClose, RouterLink],
 })
 export class HeaderComponent {
+  public isLogged = signal<boolean>(false)
+
   readonly dialog = inject(MatDialog);
+  readonly jwtSvc = inject(JwtService)
+  readonly router = inject(Router)
+
+  constructor(){
+    effect(()=>{
+      this.isLogged.set(this.jwtSvc.isLogged())
+    },{
+      allowSignalWrites:true
+    })
+  }
 
   openLoginDialog() {
     this.dialog.open(LoginDialogComponent,{
-      width:'400px'
+      width:'400px',
     });
+  }
+
+  onLogout(){
+    this.jwtSvc.removeToken()
+    this.router.navigate(['/'])
   }
 }
