42d9a95c510b976a10a26bac8e7df0d9b0ebefea
joams mogro gomez <joamsmg@gmail.com>
2024-08-01 18:19:53 -0400

    refactor: login dialog component moved

---
+++ src/app/auth/components/login-dialog/login-dialog.component.ts
@@ -0,0 +1,53 @@
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import {MatDividerModule} from '@angular/material/divider';
+import { AuthService } from '../../services';
+import { MatDialogModule } from '@angular/material/dialog';
+import { RouterLink } from '@angular/router';
+import { CustomMessageService, JwtService } from '../../../shared/services';
+import { MessageType } from '../../../shared/services/custom-message.service';
+
+@Component({
+  selector: 'app-login-dialog',
+  standalone: true,
+  imports: [
+    RouterLink,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatDialogModule
+  ],
+  templateUrl: './login-dialog.component.html',
+  styleUrl: './login-dialog.component.css'
+})
+export class LoginDialogComponent {
+  readonly fb = inject(FormBuilder)
+  readonly _snackBar = inject(CustomMessageService)
+  private readonly authSvc = inject(AuthService)
+  private readonly jwtSvc = inject(JwtService)
+
+  loginForm = this.fb.group({
+    username: ['',[Validators.required, Validators.email]],
+    password: ['',[Validators.required]]
+  })
+
+  onLogin(){
+    if(this.loginForm.invalid){
+      this.loginForm.markAllAsTouched()
+    }
+    const loginData = this.loginForm.getRawValue()
+
+    this.authSvc.signIn(loginData.username!, loginData.password!).subscribe({
+      next:r=>{
+        this.jwtSvc.setToken(r)
+        this._snackBar.showCustomMessage({message:'Inicio de sesión correcto', type:MessageType.success})
+      },
+      error:e=>console.log(e)
+    })
+  }
+}
