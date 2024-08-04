d1dfdd22208a9652ccfa64e9373d03f714c72149
joams mogro gomez <joamsmg@gmail.com>
2024-08-04 11:18:19 -0400

    feat: some material module added

--- src/app/shared/modules/material/material.module.ts
+++ src/app/shared/modules/material/material.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 
 //* you must add angular modules in imports and exports
 
 @NgModule({
   declarations: [],
-  imports: [MatGridListModule, MatButtonModule, MatCardModule],
-  exports: [MatGridListModule, MatButtonModule, MatCardModule],
+  imports: [MatGridListModule, MatButtonModule, MatCardModule, MatSidenavModule, MatDialogModule, MatIconModule],
+  exports: [MatGridListModule, MatButtonModule, MatCardModule, MatSidenavModule, MatDialogModule, MatIconModule],
 })
 export class MaterialModule {}
