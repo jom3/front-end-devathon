5f8725b604e9fa45ebf32eb36049a05208233cf7
joams mogro gomez <joamsmg@gmail.com>
2024-08-05 19:49:26 -0400

    fix: movie page mobile design added

--- src/app/features/movie/components/movie/movie.component.ts
+++ src/app/features/movie/components/movie/movie.component.ts
@@ -1,19 +1,19 @@
 import { DatePipe } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, input, Input } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { Movie } from '../../../../shared/models/movie';
+import { Movie } from '../../../../shared/models/movie.interface';
 import { MaterialModule } from '../../../../shared/modules/material/material.module';
-import { imagesBaseUrl } from '../../../../shared/services/movies.service';
 import { VideoComponent } from '../../../video/components/video/video.component';
+import { environment } from '../../../../../environments/environment.development';
 
 @Component({
-  selector: 'app-movie',
+  selector: 'movie-card',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.scss'],
   standalone: true,
   imports: [VideoComponent, RouterModule, DatePipe, MaterialModule],
 })
 export class MovieComponent {
-  public imagesBaseUrl = imagesBaseUrl;
-  @Input() movie!: Movie;
+  movie = input.required<Movie>();
+  public imagesBaseUrl = environment.imagesBaseUrl;
 }
