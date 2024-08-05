421158ffd4f65c0cdf7b48aa6df9ace521dafa33
joams mogro gomez <joamsmg@gmail.com>
2024-08-05 19:48:31 -0400

    fix: movie service fixed

--- src/app/shared/services/movies.service.ts
+++ src/app/shared/services/movies.service.ts
@@ -1,49 +1,59 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
-import { map } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
-import { Credits } from '../models/credit';
-import { Movie, Movies } from '../models/movie';
-import { Videos } from '../models/video';
+import { inject, Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { map, Observable } from "rxjs";
 
-export const imagesBaseUrl = 'https://image.tmdb.org/t/p/';
+import { environment } from "../../../environments/environment.development";
+import { Movie, MovieResponse } from "../models/movie.interface";
+import { Video, VideosResponse } from "../models/video.interface";
+import { Cast, CreditsResponse } from "../models/credit.interface";
 
 @Injectable({
   providedIn: 'root',
 })
 export class MoviesService {
-  private apiUrl = 'https://api.themoviedb.org/3';
-  private apiKey = environment.apiKEY;
-  private httpClient = inject(HttpClient);
-  constructor() {}
+  private readonly apiMovieUrl = environment.apiMovieUrl;
+  private readonly apiKey = environment.apiKEY;
+  private readonly language = environment.language;
 
-  fetchMoviesByType(type: string, pageNumber = 1) {
-    return this.httpClient.get<Movies>(
-      `${this.apiUrl}/movie/${type}?page=${pageNumber}&api_key=${this.apiKey}`
-    );
+  private readonly http = inject(HttpClient);
+
+  getNowPlayingMovies():Observable<Movie[]>{
+    return this.http.get<MovieResponse>(`${this.apiMovieUrl}/movie/now_playing?api_key=${this.apiKey}&language=${this.language}`)
+    .pipe(
+      map((result:MovieResponse)=> result.results as Movie[])
+    )
   }
 
-  fetchSimilarMovies(id: string) {
-    return this.httpClient
-      .get<Movies>(`${this.apiUrl}/movie/${id}/similar?api_key=${this.apiKey}`)
-      .pipe(map((data) => data.results));
+  getUpcomingMovies():Observable<Movie[]>{
+    return this.http.get<MovieResponse>(`${this.apiMovieUrl}/movie/upcoming?api_key=${this.apiKey}&language=${this.language}`)
+    .pipe(
+      map((result:MovieResponse)=> result.results as Movie[])
+    )
   }
 
-  fetchMovieById(id: string) {
-    return this.httpClient.get<Movie>(
-      `${this.apiUrl}/movie/${id}?api_key=${this.apiKey}`
-    );
+  getMoviesByType(type: string, page = 1):Observable<Movie[]> {
+    return this.http.get<MovieResponse>(`${this.apiMovieUrl}/movie/${type}?page=${page}&api_key=${this.apiKey}&language=${this.language}`)
+    .pipe(
+      map((result:MovieResponse)=>result.results)
+    )
   }
 
-  fetchMovieVideos(id: string) {
-    return this.httpClient
-      .get<Videos>(`${this.apiUrl}/movie/${id}/videos?api_key=${this.apiKey}`)
-      .pipe(map((data) => data.results));
+  getSimilarMovies(id: number):Observable<Movie[]> {
+    return this.http.get<MovieResponse>(`${this.apiMovieUrl}/movie/${id}/similar?api_key=${this.apiKey}&language=${this.language}`)
+      .pipe(map((result:MovieResponse) => result.results));
   }
 
-  fetchMovieCast(id: string) {
-    return this.httpClient
-      .get<Credits>(`${this.apiUrl}/movie/${id}/credits?api_key=${this.apiKey}`)
-      .pipe(map((data) => data.cast));
+  getMovieById(id: number):Observable<Movie> {
+    return this.http.get<Movie>(`${this.apiMovieUrl}/movie/${id}?api_key=${this.apiKey}&language=${this.language}`);
+  }
+
+  getMovieVideos(id: number):Observable<Video[]> {
+    return this.http.get<VideosResponse>(`${this.apiMovieUrl}/movie/${id}/videos?api_key=${this.apiKey}&language=${this.language}`)
+      .pipe(map((result:VideosResponse) => result.results));
+  }
+
+  getMovieCast(id: number):Observable<Cast[]> {
+    return this.http.get<CreditsResponse>(`${this.apiMovieUrl}/movie/${id}/credits?api_key=${this.apiKey}&language=${this.language}`)
+      .pipe(map((result:CreditsResponse) => result.cast));
   }
 }
