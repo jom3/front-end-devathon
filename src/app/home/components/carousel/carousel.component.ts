49ff790d39cb5e6d2149bab672dcd0dcf0877021
joams mogro gomez <joamsmg@gmail.com>
2024-08-05 19:49:45 -0400

    fix: carousel mobile design added

--- src/app/home/components/carousel/carousel.component.ts
+++ src/app/home/components/carousel/carousel.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, signal } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+import { SlicePipe } from '@angular/common';
+import { MoviesService } from '../../../shared/services/movies.service';
+import { Movie } from '../../../shared/models/movie.interface';
+register();
 
 @Component({
   selector: 'home-carousel',
   standalone: true,
-  imports: [],
+  imports: [SlicePipe],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './carousel.component.html',
   styleUrl: './carousel.component.css'
 })
-export class CarouselComponent {
+export class CarouselComponent implements OnInit{
+  public movies = signal<Movie[]>([])
+  private readonly movieSvc = inject(MoviesService)
 
+  ngOnInit(): void {
+    this.getNowPlayingMovies()
+  }
+
+  getNowPlayingMovies(){
+    this.movieSvc.getNowPlayingMovies().subscribe({
+      next:r=>{
+        this.movies.set(r)
+      },
+      error:e=>console.log(e)
+    })
+  }
 }
