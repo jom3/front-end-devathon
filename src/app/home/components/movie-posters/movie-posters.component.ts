1b9a519dc143e5228f31e785b8abd8643aa4f5f5
joams mogro gomez <joamsmg@gmail.com>
2024-08-05 19:50:16 -0400

    fix: movie posters mobile design added

--- src/app/home/components/movie-posters/movie-posters.component.ts
+++ src/app/home/components/movie-posters/movie-posters.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit, signal } from '@angular/core';
+import { register } from 'swiper/element/bundle';
+import { SlicePipe } from '@angular/common';
+import { Movie } from '../../../shared/models/movie.interface';
+import { MoviesService } from '../../../shared/services/movies.service';
+register();
 
 @Component({
   selector: 'home-movie-posters',
   standalone: true,
-  imports: [],
+  imports: [SlicePipe],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './movie-posters.component.html',
   styleUrl: './movie-posters.component.css'
 })
-export class MoviePostersComponent {
+export class MoviePostersComponent implements OnInit{
+  public movies = signal<Movie[]>([])
+  private readonly movieService = inject(MoviesService)
 
+  ngOnInit(): void {
+    this.getUpcomingMovies()
+  }
+
+  getUpcomingMovies(){
+    this.movieService.getUpcomingMovies().subscribe({
+      next: r=>{
+        this.movies.set(r)
+      },
+      error:e=>console.log(e)
+    })
+  }
 }
