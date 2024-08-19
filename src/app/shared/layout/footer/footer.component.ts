22dfb72deef5554327b8abe47b20c5f062319a72
joams mogro gomez <joamsmg@gmail.com>
2024-08-18 23:13:13 -0400

    feat: about us dialog option added

--- src/app/shared/layout/footer/footer.component.ts
+++ src/app/shared/layout/footer/footer.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { TermsAndConditionsComponent } from '../../components/modals/terms-and-conditions/terms-and-conditions.component';
 import { PrivacyStatementComponent } from '../../components/modals/privacy-statement/privacy-statement.component';
+import { AboutUsComponent } from '../../components/modals/about-us/about-us.component';
 
 @Component({
   selector: 'app-footer',
@@ -19,5 +20,9 @@ export class FooterComponent {
 
   openPrivacyStatement(): void {
     this.dialog.open(PrivacyStatementComponent,{});
+  }
+
+  openAboutUsDialog(): void {
+    this.dialog.open(AboutUsComponent,{});
   }
 }
