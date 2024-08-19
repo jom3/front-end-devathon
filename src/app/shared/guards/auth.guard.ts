63d0b2691fafa5e80c8b268fe48a84310354f6ec
joams mogro gomez <joamsmg@gmail.com>
2024-08-19 16:24:54 -0400

    feat: auth guard updated

--- src/app/shared/guards/auth.guard.ts
+++ src/app/shared/guards/auth.guard.ts
@@ -1,12 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { JwtService } from '../services';
 import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const isTokenActive = inject(JwtService)
+  const router = inject(Router)
+
   if(isTokenActive.getIsActiveToken()){
     return true
   }else{
+    router.navigate(['/'])
     return false
   }
 };
